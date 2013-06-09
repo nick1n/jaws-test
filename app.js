de10992bc48ca8854402fdc01a32fb668d27aee4
Nicholas Kramer <namklabs@gmail.com>
2013-06-08 19:35:48 -0500

    got dude on screen

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
-myGameState = {
-	setup: function() {
+function myGameState(){
+	var player;
+	this.setup = function() {
 		//once
-	},
-	update: function(){
+		player = new jaws.Sprite({image: 'images/player.png',x:50,y:20});
+	};
+	this.update = function(){
 		//each gametick
-	},
-	draw: function(){
+		if(jaws.pressed("left"))  { player.x--; }
+		if(jaws.pressed("right")) { player.x++; }
+	};
+	this.draw = function(){
 		//after each update
-	}
+		jaws.clear();
+		player.draw();
+	};
 }
-jaws.start(myGameState);
+jaws.onload = function(){
+	jaws.assets.add("images/player.png");
+	jaws.start(myGameState);
+}
