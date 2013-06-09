6b739259c356ae4981703227a40c04b591789a0d
Nick <nnick1n@gmail.com>
2013-06-09 16:32:30 -0500

    Made Player object and made it time based
    And lots of code cleanup.

--- app.js
+++ app.js
@@ -1,57 +1,110 @@
-var game = {
-	constants : {
-		FRICTION : 0.90,
-		GRAVITY : 1,
-		FLOOR : 336
+
+var Player = {
+
+	//states
+	STANDING: 0,
+	JUMPING: 1,
+	DBL_JUMPING: 2,
+
+
+	sprite: null,
+	vx: 0,
+	vy: 0,
+	state: null,
+	run: 1000,
+
+	setup: function() {
+		Player.sprite = new jaws.Sprite({
+			image: 'images/player.png',
+			x: jaws.width * .05,
+			y: jaws.height * .4
+		});
+
+		jaws.on_keydown("space", Player.jump);
+
+		Player.state = Player.STANDING;
 	},
-	player: null,
+
+	update: function(tick) {
+		Player.move(tick);
+		Player.grav(tick);
+	},
+
+	draw: function() {
+		Player.sprite.draw();
+	},
+
+	grav: function(tick) {
+		Player.vx *= Game.FRICTION;
+		Player.vy *= Game.FRICTION;
+
+		if (Player.sprite.y < Game.FLOOR) {
+			Player.vy -= Game.GRAVITY * tick;
+
+		} else {
+			Player.vy = 0;
+			Player.sprite.y = Game.FLOOR;
+			Player.state = Player.STANDING;
+		}
+	},
+
+	move: function(tick) {
+		if (jaws.pressed("left")) {
+			Player.sprite.x -= Player.run * tick;
+		}
+
+		if (jaws.pressed("right")) {
+			Player.sprite.x += Player.run * tick;
+		}
+
+		Player.sprite.x += Player.vx * tick;
+		Player.sprite.y -= Player.vy * tick;
+	},
+
+	jump: function() {
+		if (Player.state == Player.JUMPING) {
+			Player.vy += 2000;
+			Player.state = Player.DBL_JUMPING;
+		}
+
+		if (Player.state != Player.JUMPING && Player.state != Player.DBL_JUMPING) {
+			Player.vy += 2500;
+			Player.state = Player.JUMPING;
+		}
+	}
+
+};
+
+var Game = {
+
+	//constants
+	FRICTION: 0.90,
+	GRAVITY: 10000,
+	FLOOR: 336,
+
+
 	setup: function() {
 		//once
-		this.player = new jaws.Sprite({image: 'images/player.png',x:jaws.width * .05,y:jaws.height * .4});
-		this.player.vx = 0;
-		this.player.vy = 0;
-		this.player.state = "standing";
-		this.player.run = 10;
-		this.player.grav = function(){
-			this.vx *= game.constants.FRICTION;
-			this.vy *= game.constants.FRICTION;
-			if( game.player.y < game.constants.FLOOR ){
-				game.player.vy -= game.constants.GRAVITY;
-			} else {
-				game.player.vy = 0;
-				game.player.y = game.constants.FLOOR;
-				game.player.state = "standing";
-			}
-		}
-		this.player.move = function(){
-			this.x += this.vx;
-			this.y -= this.vy;
-			this.grav();
-		}
-		jaws.on_keydown("space", function(){
-			if(this.player.state == "jumping"){
-				this.player.vy += 20;
-				this.player.state = "dbljumping";
-			}
-			if(this.player.state != "jumping" && this.player.state != "dbljumping"){
-				this.player.vy += 25;
-				this.player.state = "jumping";
-			}
-		}.bind(this));
+		Player.setup();
 	},
-	update: function(){
+
+	update: function() {
+		var tick = jaws.game_loop.tick_duration / 1000;
+
 		//each gametick
-		if(jaws.pressed("left") && jaws.pressed)  { this.player.x -= this.player.run; }
-		if(jaws.pressed("right")) { this.player.x += this.player.run; }
-		this.player.move();
+		Player.update(tick);
 	},
-	draw: function(){
+
+	draw: function() {
 		//after each update
 		jaws.clear();
-		this.player.draw();
+
+		Player.draw();
 	}
+
 };
-jaws.onload = function(){
+
+jaws.onload = function() {
 	jaws.assets.add("images/player.png");
-	jaws.start(game);
+	jaws.start(Game);
 }
