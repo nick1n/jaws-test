d9b147fe02ea5583fc19bdfd0ce20d5f40184223
Nicholas Kramer <namklabs@gmail.com>
2013-06-08 20:13:14 -0500

    changed app.js to object style

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
-function myGameState(){
-	var player;
-	this.setup = function() {
+var game = {
+	player: null,
+	setup: function() {
 		//once
-		player = new jaws.Sprite({image: 'images/player.png',x:50,y:20});
-	};
-	this.update = function(){
+		this.player = new jaws.Sprite({image: 'images/player.png',x:jaws.width * .05,y:jaws.height * .8});
+	},
+	update: function(){
 		//each gametick
-		if(jaws.pressed("left"))  { player.x--; }
-		if(jaws.pressed("right")) { player.x++; }
-	};
-	this.draw = function(){
+		if(jaws.pressed("left"))  { this.player.x--; }
+		if(jaws.pressed("right")) { this.player.x++; }
+	},
+	draw: function(){
 		//after each update
 		jaws.clear();
-		player.draw();
-	};
-}
+		this.player.draw();
+	}
+};
 jaws.onload = function(){
 	jaws.assets.add("images/player.png");
-	jaws.start(myGameState);
+	jaws.start(game);
 }
