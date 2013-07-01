1a44ad092eb20c4c08c86f0bbe32a4d2ef27f862
Nicholas Ness <nnick1n@gmail.com>
2013-07-01 02:05:26 -0500

    A few minor improvements
    Added a simple Keyboard utility class, added a box that does nothing
    and doesn't even look like a box, and made the movement kind of better

--- app.js
+++ app.js
@@ -1,4 +1,50 @@
 
+var Keyboard = {
+
+	// states
+	UP: 1,
+	DOWN: 2,
+
+	keys: {},
+
+	setup: function() {
+
+		$(document).on('keydown', Keyboard.down);
+		$(document).on('keypress', Keyboard.press);
+		$(document).on('keyup', Keyboard.up);
+
+	},
+
+	down: function(event) {
+		var which = event.which;
+
+		if (Keyboard.keys[which]) {
+			if (Keyboard.keys[which].state == Keyboard.UP) {
+				Keyboard.keys[which].state = Keyboard.DOWN;
+				Keyboard.keys[which].callback();
+			}
+		}
+	},
+
+	press: function(event) {
+
+	},
+
+	up: function(event) {
+		var which = event.which;
+
+		if (Keyboard.keys[which]) {
+			Keyboard.keys[which].state = Keyboard.UP;
+		}
+	},
+
+	register: function(which, callback) {
+		Keyboard.keys[which] = {};
+		Keyboard.keys[which].state = Keyboard.UP;
+		Keyboard.keys[which].callback = callback;
+	}
+};
+
 var Player = {
 
 	//states
@@ -11,7 +57,10 @@ var Player = {
 	vx: 0,
 	vy: 0,
 	state: null,
-	run: 1000,
+	run: 750,
+	accelerate: 0.7,
+	speed: 0,
+
 
 	setup: function() {
 		Player.sprite = new jaws.Sprite({
@@ -20,7 +69,9 @@ var Player = {
 			y: jaws.height * .4
 		});
 
-		jaws.on_keydown("space", Player.jump);
+		Keyboard.setup();
+		Keyboard.register(32, Player.jump);
+		//jaws.on_keydown("space", Player.jump);
 
 		Player.state = Player.STANDING;
 	},
@@ -35,8 +86,6 @@ var Player = {
 	},
 
 	grav: function(tick) {
-		Player.vx *= Game.FRICTION;
-		Player.vy *= Game.FRICTION;
 
 		if (Player.sprite.y < Game.FLOOR) {
 			Player.vy -= Game.GRAVITY * tick;
@@ -49,28 +98,59 @@ var Player = {
 	},
 
 	move: function(tick) {
+
 		if (jaws.pressed("left")) {
-			Player.sprite.x -= Player.run * tick;
+			Player.speed = -Player.run;
+
+		} else if (jaws.pressed("right")) {
+			Player.speed = Player.run;
+
+		} else {
+			Player.speed = 0;
 		}
 
-		if (jaws.pressed("right")) {
-			Player.sprite.x += Player.run * tick;
+		Player.vx = Player.accelerate * Player.speed * tick + (1 - Player.accelerate) * Player.vx;
+		if (Math.abs(Player.vx) < 0.0001) {
+			Player.vx = 0;
 		}
 
-		Player.sprite.x += Player.vx * tick;
+		Player.sprite.x += Player.vx;
 		Player.sprite.y -= Player.vy * tick;
 	},
 
 	jump: function() {
+
 		if (Player.state == Player.JUMPING) {
-			Player.vy += 2000;
+			Player.vy = 1000;
 			Player.state = Player.DBL_JUMPING;
 		}
 
 		if (Player.state != Player.JUMPING && Player.state != Player.DBL_JUMPING) {
-			Player.vy += 2500;
+			Player.vy = 1500;
 			Player.state = Player.JUMPING;
 		}
+	}
+
+};
+
+var Box = {
+
+	sprite: null,
+
+	setup: function() {
+
+		Box.sprite = new jaws.Sprite({
+			image: 'images/player.png',
+			x: jaws.width * .5,
+			y: jaws.height * .6
+		});
+
+	},
+
+	update: function(tick) {},
+
+	draw: function() {
+		Box.sprite.draw();
 	}
 
 };
@@ -78,7 +158,7 @@ var Player = {
 var Game = {
 
 	//constants
-	FRICTION: 0.90,
+	FRICTION: 0.70,
 	GRAVITY: 10000,
 	FLOOR: 336,
 
@@ -86,6 +166,7 @@ var Game = {
 	setup: function() {
 		//once
 		Player.setup();
+		Box.setup();
 	},
 
 	update: function() {
@@ -93,6 +174,7 @@ var Game = {
 
 		//each gametick
 		Player.update(tick);
+		Box.update(tick);
 	},
 
 	draw: function() {
@@ -100,6 +182,7 @@ var Game = {
 		jaws.clear();
 
 		Player.draw();
+		Box.draw();
 	}
 
 };
